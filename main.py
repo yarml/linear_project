1a8f0a431be6c0192fb71c40e75ceadd04a3d9ce
yarml <youssefharmal@gmail.com>
2024-04-29 10:21:14 +0100

    Method 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from scipy.signal import convolve2d # Apparently NumPy cannot convolve matrices
 
 def image_load(path: str) -> np.ndarray:
   # This would load the image into a tensor where an index [y][x][rgba] would determin
@@ -16,7 +17,7 @@ def reduce_to_grayscale(tensor: np.ndarray) -> np.array:
   row = (tensor[0] + tensor[1] + tensor[2]) / 3
   return np.asarray([row, row, row]).astype(np.uint8)
 def reduce_to_bin(tensor: np.ndarray, threshold: np.uint8) -> np.ndarray:
-  row = np.where((tensor[0] + tensor[1] + tensor[2]) / 3 < threshold, 255, 0)
+  row = np.where((tensor[0] + tensor[1] + tensor[2]) / 3 < threshold, 0, 255)
   return np.asarray([row, row, row]).astype(np.uint8)
 
 def diff(f0: np.ndarray, f1: np.ndarray) -> np.ndarray:
@@ -25,6 +26,16 @@ def add(f0: np.ndarray, f1: np.ndarray) -> np.ndarray:
   return (f1 + f0).astype(np.uint8)
 def invert(tensor: np.ndarray) -> np.ndarray:
   return 255 - tensor
+
+def image_convolve(matrix, kernel):
+	width = matrix[0].shape[0]
+	height = matrix[0].shape[1]
+	output_matrix = np.zeros((3, width, height))
+	
+	for ch in range(3):
+		channel = matrix[ch]
+		output_matrix[ch] = convolve2d(channel, kernel, mode='same', boundary='fill', fillvalue=0)
+	return output_matrix.astype(np.uint8)
 
 # Method 1:
 # Because laziness, the frames are not separated by us, they should be in the files frame0.png and frame1.png
@@ -60,12 +71,18 @@ def method2():
   nshifted = np.roll(source, (-shift_amnt, -shift_amnt), (1, 2))
   inv_nshifted = invert(nshifted)
   nshift_org = np.delete(np.delete(add(source/2, inv_nshifted/2), rm_indices, 1), rm_indices, 2)
-  image_save(pshift_org, "pshift.jpeg")
-  image_save(nshift_org, "nshift.jpeg")
+  #image_save(pshift_org, "pshift.jpeg")
+  #image_save(nshift_org, "nshift.jpeg")
   output = reduce_to_bin(add(nshift_org, pshift_org), 1)
   image_save(output, "output.jpeg")
 
+def method3():
+  source = image_load("frame0.png")
+  source = reduce_to_grayscale(source)
+  output = reduce_to_bin(image_convolve(source, np.array([[-0.1, 0, 0.1], [-0.2, 0, 0.2], [-0.1, 0, 0.1]])), 60)
+  image_save(output, "output.jpeg")
 
 #method1()
 #method1p1()
-method2()
+#method2()
+method3()
